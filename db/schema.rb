9bd7e1492169aa53d688f069323a1ac44017540a
donrow007 <wn.nw106@gmail.com>
2022-09-04 14:25:36 +0900

    エラーの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_02_193539) do
+ActiveRecord::Schema.define(version: 2022_09_03_230254) do
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "room_id", null: false
