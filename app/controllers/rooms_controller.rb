9da5162589cde38fa6b8f730426fc7594048b026
donrow007 <wn.nw106@gmail.com>
2022-09-04 11:16:04 +0900

    ルームユーザー＋新規作成の修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,5 @@
 class RoomsController < ApplicationController
-
   def index
-  end
-  
-  def new
-    @room = Room.new
   end
 
   def create
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,11 +1,7 @@
 class Room < ApplicationRecord
-<<<<<<< HEAD
   has_many :room_users
   has_many :users, through: :room_users
 
   validates :name, presence: true
 
 end
-=======
-end
->>>>>>> parent of e34cdb6 (room_usersテーブルの作成)
