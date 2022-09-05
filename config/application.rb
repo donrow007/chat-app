ddca5408f35c4e662359d80c37ea0dcf555f1049
donrow007 <wn.nw106@gmail.com>
2022-09-05 23:00:11 +0900

    投稿時刻の日本時間への修正

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,10 @@ Bundler.require(*Rails.groups)
 module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-     # 中略
+    config.load_defaults 6.0 
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+   # 中略
       config.generators do |g|
         g.stylesheets false
         g.javascripts false
